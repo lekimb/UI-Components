637c5c164e62b92e7786babf42f742818dc2201d
Mikel <lem_bar@hotmail.com>
2022-12-19 10:26:42 +0100

    Notification and price card first commit

---
+++ Notification component/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./img/**/*.{svg}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ Price card component/src/App.js
@@ -0,0 +1,12 @@
+const useState = React.useState;
+
+function App() {
+
+    return (
+        
+        <div>JSX React Component</div>
+
+    )  
+}
+
+export default App;
